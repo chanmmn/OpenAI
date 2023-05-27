293237b24fe2600e704ee651fcc947bfd00f0c4c
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-05-27 21:49:59 +0800

    Commit ConAppChatGPT
    Commit ConAppChatGPT

---
+++ ConAppChatGPT/ConAppChatGPT/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenAI_API;
+
+namespace ConAppChatGPT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the API key
+            var openAi = new OpenAIAPI("OPENAI Key");
+
+            var completions = openAi.Completions.CreateCompletionAsync(
+    prompt: "1 + 1=",
+    model: "text-davinci-002",
+    max_tokens: 100,
+    temperature: 1f
+);
+            foreach (var completion in completions.Result.Completions)
+            {
+                Console.Write(completion.Text);
+            }
+
+        }
+    }
+}
